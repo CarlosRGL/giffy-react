c89a90becdeffc58528e311b89876e8fa576230e
CarlosRGL <carlos@quai13.com>
2020-07-16 11:22:09 +0200

    📦 NEW: added request giphy api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import getGifs from './services/getGifs';
 
 function App() {
+  const [gif, setGif] = useState([])
+  useEffect(function () {
+    getGifs().then(gifs => setGif(gifs))
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="App-content">
+        {
+          gif.map(singleGif => <img alt="gif" src={singleGif} />)
+        }
+      </section>
     </div>
   );
 }
